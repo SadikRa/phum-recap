6ed70bf75c195f3cbb70911e983fc3fa0a54da57
SadikRa <sadikrahman494@gmail.com>
2025-01-22 14:48:11 +0600

    create coures done

--- src/redux/features/admin/courseManagement.ts
+++ src/redux/features/admin/courseManagement.ts
@@ -19,6 +19,7 @@ const courseManagement = baseApi.injectEndpoints({
           params: params,
         };
       },
+      providesTags: ["semester"],
       transformResponse: (response: TResponseRedux<TSemester[]>) => {
         return {
           data: response.data,
@@ -33,14 +34,53 @@ const courseManagement = baseApi.injectEndpoints({
         method: "POST",
         body: data,
       }),
+      invalidatesTags: ["semester"],
     }),
+
     updateRegisteredSemester: builder.mutation({
       query: (args) => ({
         url: `/semester-registrations/${args.id}`,
         method: "PATCH",
         body: args.data,
       }),
+      invalidatesTags: ["semester"],
     }),
+
+
+    getAllCourses: builder.query({
+      query: (args) => {
+        const params = new URLSearchParams();
+
+        if (args) {
+          args.forEach((item: TQueryParam) => {
+            params.append(item.name, item.value as string);
+          });
+        }
+
+        return {
+          url: "/courses",
+          method: "GET",
+          params: params,
+        };
+      },
+      providesTags: ["semester"],
+      transformResponse: (response: TResponseRedux<any>) => {
+        return {
+          data: response.data,
+          meta: response.meta,
+        };
+      },
+    }),
+
+    addCourse: builder.mutation({
+      query: (data) => ({
+        url: `/courses/create-course`,
+        method: 'POST',
+        body: data,
+      }),
+      invalidatesTags: ['courses'],
+    }),
+
   }),
 });
 
@@ -48,4 +88,6 @@ export const {
   useAddRegisteredSemesterMutation,
   useGetAllRegisteredSemestersQuery,
   useUpdateRegisteredSemesterMutation,
+  useGetAllCoursesQuery,
+  useAddCourseMutation
 } = courseManagement;
